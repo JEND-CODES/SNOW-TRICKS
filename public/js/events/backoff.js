00307d712dee72bffc308515c4718e2e48d3d04b
JEND-CODES <54572997+JEND-CODES@users.noreply.github.com>
2021-03-02 23:34:55 +0100

    Update backoff.js

--- public/js/events/backoff.js
+++ public/js/events/backoff.js
@@ -4,13 +4,13 @@ $(document).ready(function () {
 
     var x = 6;
 
-    $('#trick-box .load-more:lt('+x+')').show();
+    $("#trick-box .load-more:lt("+x+")").show();
 
-        $('#more-btn').click(function () {
+        $("#more-btn").click(function () {
 
         x = (x + 3 <= totalTricks) ? x + 3 : totalTricks;
 
-        $('#trick-box .load-more:lt('+x+')').slideDown();
+        $("#trick-box .load-more:lt("+x+")").slideDown();
 
     });
 
