1d5610478e53ad27fbd236e93581c75473e8a9e8
JEND-CODES <54572997+JEND-CODES@users.noreply.github.com>
2021-03-02 23:42:26 +0100

    Update app.js

--- public/js/vue/app.js
+++ public/js/vue/app.js
@@ -1,7 +1,7 @@
 new Vue({
-    el: '#appJEND',
-    // Changement des délimiteurs pour l'interpolation des variables (cause conflits avec TWIG)
-    delimiters: ['[+[', ']+]'],
+    el: "#appJEND",
+    // Changement des délimiteurs pour l"interpolation des variables (cause conflits avec TWIG)
+    delimiters: ["[+[", "]+]"],
     
     vuetify: new Vuetify(),
 
@@ -18,48 +18,48 @@ new Vue({
             searching: false,
 
             // Modals on posts medias Flex Grid items
-            modal_screen_1: false,
-            modal_screen_2: false,
-            modal_screen_3: false,
-            modal_screen_4: false,
-            modal_screen_5: false,
-            modal_screen_6: false,
-            modal_screen_7: false,
-            modal_screen_8: false,
-            modal_screen_9: false,
-            modal_screen_10: false,
-            modal_screen_11: false,
-            modal_screen_12: false,
-            modal_screen_13: false,
-            modal_screen_14: false,
-            modal_screen_15: false,
-            modal_screen_16: false,
-            modal_screen_17: false,
-            modal_screen_18: false,
-            modal_screen_19: false,
-            modal_screen_20: false,
+            modalScreen1: false,
+            modalScreen2: false,
+            modalScreen3: false,
+            modalScreen4: false,
+            modalScreen5: false,
+            modalScreen6: false,
+            modalScreen7: false,
+            modalScreen8: false,
+            modalScreen9: false,
+            modalScreen10: false,
+            modalScreen11: false,
+            modalScreen12: false,
+            modalScreen13: false,
+            modalScreen14: false,
+            modalScreen15: false,
+            modalScreen16: false,
+            modalScreen17: false,
+            modalScreen18: false,
+            modalScreen19: false,
+            modalScreen20: false,
 
             // Modals on posts medias Flex Grid items
-            modal_update_1: false,
-            modal_update_2: false,
-            modal_update_3: false,
-            modal_update_4: false,
-            modal_update_5: false,
-            modal_update_6: false,
-            modal_update_7: false,
-            modal_update_8: false,
-            modal_update_9: false,
-            modal_update_10: false,
-            modal_update_11: false,
-            modal_update_12: false,
-            modal_update_13: false,
-            modal_update_14: false,
-            modal_update_15: false,
-            modal_update_16: false,
-            modal_update_17: false,
-            modal_update_18: false,
-            modal_update_19: false,
-            modal_update_20: false,
+            modalUpdate1: false,
+            modalUpdate2: false,
+            modalUpdate3: false,
+            modalUpdate4: false,
+            modalUpdate5: false,
+            modalUpdate6: false,
+            modalUpdate7: false,
+            modalUpdate8: false,
+            modalUpdate9: false,
+            modalUpdate10: false,
+            modalUpdate11: false,
+            modalUpdate12: false,
+            modalUpdate13: false,
+            modalUpdate14: false,
+            modalUpdate15: false,
+            modalUpdate16: false,
+            modalUpdate17: false,
+            modalUpdate18: false,
+            modalUpdate19: false,
+            modalUpdate20: false,
 
             // modal_1: false,
             // modal_2: false,
@@ -74,55 +74,55 @@ new Vue({
 
             // Dropdown Nav Links
             // navlinks: [{
-            //         title: 'Login'
+            //         title: "Login"
             //     },
             //     {
-            //         title: 'Back Office'
+            //         title: "Back Office"
             //     },
             //     {
-            //         title: 'Click Me 3'
+            //         title: "Click Me 3"
             //     },
             //     {
-            //         title: 'Click Me 4'
+            //         title: "Click Me 4"
             //     },
             // ],
 
             // colors: [
-            //     'indigo lighten-3',
-            //     'indigo lighten-2',
-            //     'indigo lighten-1',
-            //     'deep-purple lighten-3',
-            //     'deep-purple lighten-2',
+            //     "indigo lighten-3",
+            //     "indigo lighten-2",
+            //     "indigo lighten-1",
+            //     "deep-purple lighten-3",
+            //     "deep-purple lighten-2",
             // ],
             // Slideshow titles test
             // slides: [
-            //     '1er',
-            //     '2ème',
-            //     '3ème',
-            //     '4ème',
-            //     '5ème',
+            //     "1er",
+            //     "2ème",
+            //     "3ème",
+            //     "4ème",
+            //     "5ème",
             // ],
-            // Messages d'alertes
-            // info: 'INFO1',
-            // info2: 'INFO2',
-            // info3: 'INFO3',
-            // info4: 'INFO4',
-            // link: 'http://planetcode.fr',
+            // Messages d"alertes
+            // info: "INFO1",
+            // info2: "INFO2",
+            // info3: "INFO3",
+            // info4: "INFO4",
+            // link: "http://planetcode.fr",
 
             // Navigation drawer component :
             drawer: null,
 
-            // Utiliser le v-if pour afficher un bloc, false-> ne s'affiche pas, true-> s'affiche :
+            // Utiliser le v-if pour afficher un bloc, false-> ne s"affiche pas, true-> s"affiche :
             successful: true,
-            // Placement d'un v-else qui s'affiche ou se masque en fonction de la valeur true/false de "successful
+            // Placement d"un v-else qui s"affiche ou se masque en fonction de la valeur true/false de "successful
 
-            // Utilisation d'un v-for pour afficher la liste d'un array qui crée autant de <li> qu'il y a de noms dans l'array :
+            // Utilisation d"un v-for pour afficher la liste d"un array qui crée autant de <li> qu"il y a de noms dans l"array :
             // persons: [
-            //     'Marie',
-            //     'Marion',
-            //     'Michel',
-            //     'Olivier',
-            //     'Jean',
+            //     "Marie",
+            //     "Marion",
+            //     "Michel",
+            //     "Olivier",
+            //     "Jean",
             // ]
         }
     },
@@ -140,7 +140,7 @@ new Vue({
 
         // Scroll on top function :
         onScroll(e) {
-            if (typeof window === 'undefined') return
+            if (typeof window === "undefined") return
             const top = window.pageYOffset || e.target.scrollTop || 0
             this.fab = top > 20
         },
@@ -148,21 +148,21 @@ new Vue({
             this.$vuetify.goTo(0)
         },
 
-        // V-bind exemple https://codepen.io/minamo173/pen/aEdYpp?editors=1010 : Il suffit ensuite d'ajouter par exemple :v-bind:style="setColor('orange')" à un component...
+        // V-bind exemple https://codepen.io/minamo173/pen/aEdYpp?editors=1010 : Il suffit ensuite d"ajouter par exemple :v-bind:style="setColor("orange")" à un component...
         // setColor(color) {
         //     let style
         //     switch (color) {
-        //         case 'red':
+        //         case "red":
         //             style = {
         //                 "background-color": "#f44336"
         //             }
         //             break
-        //         case 'blue':
+        //         case "blue":
         //             style = {
         //                 "background-color": "#3f51b5"
         //             }
         //             break
-        //         case 'orange':
+        //         case "orange":
         //             style = {
         //                 "background-color": "#ff9800"
         //             }
