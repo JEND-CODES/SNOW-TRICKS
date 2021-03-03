02934a6b23abd135fd55114aa9fa138eaadb9d7a
JEND-CODES <54572997+JEND-CODES@users.noreply.github.com>
2021-03-03 13:40:35 +0100

    Update event.js

--- public/js/events/event.js
+++ public/js/events/event.js
@@ -1,29 +1,29 @@
 $(document).ready(function () {
 
-    $("#spinner").delay(1000).css('display', 'none');
-    $('.control-medias-container').css('display', 'none');
-    $('.control-medias').hide();
-    $('.control-medias-2').hide();
+    $("#spinner").delay(1000).css("display", "none");
+    $(".control-medias-container").css("display", "none");
+    $(".control-medias").hide();
+    $(".control-medias-2").hide();
 
     if (window.matchMedia("(max-width: 959px)").matches) {
 
-        $('.control-medias-container').css('display', 'initial');
-        $('.medias-grid').hide();
-        $('.control-medias').show();
+        $(".control-medias-container").css("display", "initial");
+        $(".medias-grid").hide();
+        $(".control-medias").show();
 
         $(".control-medias").click(function () {
     
-            $('.medias-grid').show();
-            $('.control-medias').hide();
-            $('.control-medias-2').show();
+            $(".medias-grid").show();
+            $(".control-medias").hide();
+            $(".control-medias-2").show();
 
         });
 
         $(".control-medias-2").click(function () {
     
-            $('.medias-grid').hide();
-            $('.control-medias-2').hide();
-            $('.control-medias').show();
+            $(".medias-grid").hide();
+            $(".control-medias-2").hide();
+            $(".control-medias").show();
         
         });
 
